61c78373458360ee558f424ec74774b72c5e9058
Rushabh Mukkirwar <rushabhmukkirwar@gmail.com>
2020-07-25 12:12:08 +0530

    Some necessary changes

--- src/app.js
+++ src/app.js
@@ -23,14 +23,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Rushabh Mukkirwar'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Rushabh Mukkirwar'
     })
 })
 
@@ -38,7 +38,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Rushabh Mukkirwar'
     })
 })
 
@@ -70,7 +70,7 @@ app.get('/weather', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Rushabh Mukkirwar',
         errorMessage: 'Help article not found.'
     })
 })
@@ -78,7 +78,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Rushabh Mukkirwar',
         errorMessage: 'Page not found.'
     })
 })
